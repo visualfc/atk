bfa49c807418bdfda4b5dcbb336f99e8e263b6a0
visualfc <visualfc@gmail.com>
2021-01-21 22:18:13 +0800

    tk for windows auto set high dpi

--- tk/window_windows.go
+++ tk/window_windows.go
@@ -2,7 +2,10 @@
 
 package tk
 
-import "fmt"
+import (
+	"fmt"
+	"syscall"
+)
 
 func (w *Window) ShowMaximized() error {
 	return eval(fmt.Sprintf("wm state %v zoomed", w.id))
@@ -11,4 +14,26 @@ func (w *Window) ShowMaximized() error {
 func (w *Window) IsMaximized() bool {
 	r, _ := evalAsString(fmt.Sprintf("wm state %v", w.id))
 	return r == "zoomed"
+}
+
+func init() {
+	registerInit(setHighDPI)
+}
+
+func setHighDPI() {
+	dll, err := syscall.LoadDLL("Shcore.dll")
+	if err != nil {
+		return
+	}
+	procSetProcessDpiAwareness, err := dll.FindProc("SetProcessDpiAwareness")
+	if err != nil {
+		return
+	}
+	procGetScaleFactorForDevice, err := dll.FindProc("GetScaleFactorForDevice")
+	if err != nil {
+		return
+	}
+	procSetProcessDpiAwareness.Call(1)
+	r0, _, _ := procGetScaleFactorForDevice.Call()
+	mainInterp.Eval(fmt.Sprintf("tk scaling %v", float64(r0)/75))
 }
