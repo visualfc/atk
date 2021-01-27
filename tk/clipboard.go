2c92404729b24f1f3b82b63449926e235e0121d3
visualfc <visualfc@gmail.com>
2021-01-27 22:06:59 +0800

    tk: add GetClipboardText

--- tk/clipboard.go
+++ tk/clipboard.go
@@ -1,13 +1,20 @@
+// Copyright 2018 visualfc. All rights reserved.
+
 package tk
 
 import "fmt"
 
 func ClearClipboard() {
-	evalAsString("clipboard clear")
+	eval("clipboard clear")
 }
 
-func AppendToClipboard(text string) {
+func AppendToClipboard(text string) error {
 	pname := "atk_tmp_clip"
 	setObjText(pname, text)
-	eval(fmt.Sprintf("clipboard append $%v", pname))
+	return eval(fmt.Sprintf("clipboard append $%v", pname))
+}
+
+func GetClipboardText() string {
+	text, _ := evalAsString("clipboard get -type UTF8_STRING")
+	return text
 }
